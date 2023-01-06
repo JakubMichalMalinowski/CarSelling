678799b2569562d897e376cfc4313da506d0a567
Jakub <jakub-malinowski@outlook.com>
2023-01-06 22:08:55 +0100

    Add MVC project to consume API

---
+++ CarSellingApp/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using CarSelling.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CarSellingApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
