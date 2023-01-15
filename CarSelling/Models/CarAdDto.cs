62bb3727a166ae8a04b032a6efc50256fff39d86
Jakub <jakub-malinowski@outlook.com>
2023-01-15 10:32:50 +0100

    Create DTOs

---
+++ CarSelling/Models/CarAdDto.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarSelling.Models
+{
+    public class CarAdDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public string? PhotoPath { get; set; }
+
+        [Required]
+        [Precision(18, 2)]
+        [Range(0, double.MaxValue)]
+        public decimal Price { get; set; }
+
+        [Required]
+        public bool Negotiable { get; set; }
+
+        [Required]
+        public Owner Owner { get; set; } = default!;
+    }
+}
