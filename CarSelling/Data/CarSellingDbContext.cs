60ae5e78c5c68a6d3f6360174aa81f6a9cf85604
Jakub <jakub-malinowski@outlook.com>
2023-06-09 16:43:01 +0200

    Add defaults to DbSets in AppDbContext

--- CarSelling/Data/CarSellingDbContext.cs
+++ CarSelling/Data/CarSellingDbContext.cs
@@ -7,10 +7,10 @@ namespace CarSelling.Data
     {
         public CarSellingDbContext(DbContextOptions<CarSellingDbContext> options) : base(options) { }
 
-        public DbSet<CarAd> CarAds { get; set; }
-        public DbSet<User> Users { get; set; }
-        public DbSet<Car> Cars { get; set; }
-        public DbSet<FilePath> FilePaths { get; set; }
-        public DbSet<EncodedFile> EncodedFiles { get; set; }
+        public DbSet<CarAd> CarAds { get; set; } = default!;
+        public DbSet<User> Users { get; set; } = default!;
+        public DbSet<Car> Cars { get; set; } = default!;
+        public DbSet<FilePath> FilePaths { get; set; } = default!;
+        public DbSet<EncodedFile> EncodedFiles { get; set; } = default!;
     }
 }
